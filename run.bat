0e8ffbecad8f412d5a91db4abe7787a2aa0c30ed
Dong <yonghui.dong@gmail.com>
2023-08-23 11:14:59 +0300

    init

--- run.bat
+++ run.bat
@@ -1,10 +1,7 @@
-@echo %off
+@echo off
 setlocal enabledelayedexpansion
-
 set AppDir=%~dp0
 set R="%AppDir%R\bin\x64\R.exe"
 set runfile="%AppDir%run.R"
-set appfolder="%AppDir%app"
-
-%R% --no-save --slave -f %runfile% --args %appfolder%
+%R% --no-save --slave -f %runfile%
 exit 0
